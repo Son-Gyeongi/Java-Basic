b7ba224fabe924092378bd386cefbd9aa970d990
Son-Gyeongi <thsruddl77@gmail.com>
2025-08-28 22:52:50 +0900

    section9 62. final 변수와 상수2

---
+++ src/final1/Constant.java
@@ -0,0 +1,19 @@
+package final1;
+
+/*
+상수
+- 변하지 않고, 항상 일정한 값을 갖는 수를 말한다.
+- 자바에서는 단 하나만 존재하는 변하지 않는 고정된 값
+- 애플리케이션 실행되는 동안 변하지 않음
+- static final 키워드 사용
+ */
+public class Constant {
+    // 수학 상수
+    public static final double PI = 3.14;
+    // 시간 상수
+    public static final int HOUR_IN_DAY = 24;
+    public static final int MINUTES_IN_HOUR = 60;
+    public static final int SECONDS_IN_MINUTE = 60;
+    // 애플리케이션 설정 상수
+    public static final int MAX_USERS = 2000;
+}
