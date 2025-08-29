d2f7760e703439f987e215f7beb76cf383fca73b
Son-Gyeongi <thsruddl77@gmail.com>
2025-08-29 22:30:53 +0900

    section10 65. 상속 - 시작

---
+++ src/extends1/ex1/ElectricCar.java
@@ -0,0 +1,14 @@
+package extends1.ex1;
+
+// 자동차 객체
+// 전기차
+public class ElectricCar {
+
+    public void move() {
+        System.out.println("차를 이동합니다.");
+    }
+
+    public void charge() {
+        System.out.println("충전합니다.");
+    }
+}
---
+++ src/extends1/ex1/GasCar.java
@@ -0,0 +1,14 @@
+package extends1.ex1;
+
+// 자동차 객체
+// 일반 휘발유 차(가솔린차)
+public class GasCar {
+
+    public void move() {
+        System.out.println("차를 이동합니다.");
+    }
+
+    public void fillUp() {
+        System.out.println("기름을 주유합니다.");
+    }
+}
