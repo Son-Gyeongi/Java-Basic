81bc25d99060937b8b8ad5b708e6e54ae8bddb15
Son-Gyeongi <thsruddl77@gmail.com>
2025-09-05 21:34:41 +0900

    section12 82. 다형성 활용1

---
+++ src/poly/ex1/AnimalSoundMain.java
@@ -0,0 +1,34 @@
+package poly.ex1;
+
+public class AnimalSoundMain {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Cow cow = new Cow();
+        Cow cow1 = new Cow();
+        Cow cow2 = new Cow();
+
+        // Cow[] cowArr = {dog, cat, cow}; 안됨, 타입이 다르다.
+        Cow[] cowArr = {cow, cow1, cow2};
+
+        System.out.println("동물 소리 테스트 시작");
+        dog.sound();
+        System.out.println("동물 소리 테스트 종료");
+
+        soundCat(cat);
+        soundCow(cow);
+    }
+
+    private static void soundCat(Cat cat) {
+        System.out.println("동물 소리 테스트 시작");
+        cat.sound();
+        System.out.println("동물 소리 테스트 종료");
+    }
+
+    private static void soundCow(Cow cow) {
+        System.out.println("동물 소리 테스트 시작");
+        cow.sound();
+        System.out.println("동물 소리 테스트 종료");
+    }
+}
